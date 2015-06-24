e865df63be31d4782ce068d1f429fa6008727db6
XEROpw <xero@xero.pw>
2015-06-24 17:14:49 -0400

    GUI
    Now the script has a GUI.

--- pw/xero/parabot/thieving/XEROthieving.java
+++ pw/xero/parabot/thieving/XEROthieving.java
@@ -1,10 +1,19 @@
 package pw.xero.parabot.thieving;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import org.parabot.environment.api.interfaces.Paintable;
 import org.parabot.environment.api.utils.Time;
+import org.parabot.environment.api.utils.Timer;
 import org.parabot.environment.scripts.Category;
 import org.parabot.environment.scripts.Script;
 import org.parabot.environment.scripts.ScriptManifest;
@@ -28,6 +37,11 @@ import org.rev317.min.api.wrappers.Item;
 public class XEROthieving extends Script implements Paintable, MessageListener
 {
 	ArrayList<Strategy> strategies = new ArrayList<Strategy>();
+	private final Image chat = getImage("http://parabot.xero.pw/img/thieving.jpg");
+	
+	private int money, steals, randoms, oldstack = 0;
+	private Timer runtime = new Timer();
+	int location = 0;
 	
 	@Override
 	public boolean onExecute()
@@ -55,8 +69,8 @@ public class XEROthieving extends Script implements Paintable, MessageListener
 		case 1: // ARDOUGNE
 			ArdougneZone ArdougneZone = new ArdougneZone();
 			strategies.add(ArdougneZone.location);
-			strategies.add(new Deposit());
 			strategies.add(ArdougneZone.steal);
+			location = 1;
 			break;
 		case 2: // DRAYNOR-STALLS
 			DraynorZone DraynorZone = new DraynorZone(gui.doBank.isSelected());
@@ -65,6 +79,7 @@ public class XEROthieving extends Script implements Paintable, MessageListener
 			if(gui.doBank.isSelected()) strategies.add(DraynorZone.bank);
 			else strategies.add(DraynorZone.empty);
 			strategies.add(DraynorZone.stall);
+			location = 2;
 			break;
 		case 3: // DRAYNOR-FARMER
 			DraynorZone DraynorZone1 = new DraynorZone(gui.doBank.isSelected());
@@ -73,17 +88,23 @@ public class XEROthieving extends Script implements Paintable, MessageListener
 			if(gui.doBank.isSelected()) strategies.add(DraynorZone1.bank);
 			else strategies.add(DraynorZone1.empty);
 			strategies.add(DraynorZone1.farmer);
+			location = 3;
 			break;
 		case 4: // DONATOR
 			DonatorZone DonatorZone = new DonatorZone();
 			strategies.add(DonatorZone.location);
 			strategies.add(DonatorZone.steal);
+			location = 4;
 			break;
 		default:
 			return false;
 		}
 		
 		provide(strategies);
+		steals = 0;
+		randoms = 0;
+		money = 0;
+		oldstack = 0;
 		return true;
 	};
 	
@@ -94,16 +115,92 @@ public class XEROthieving extends Script implements Paintable, MessageListener
 	}
 	
 	@Override
-	public void messageReceived(MessageEvent arg0)
+	public void messageReceived(MessageEvent me)
 	{
-		// TODO Auto-generated method stub
-		
+		String message = me.getMessage().toLowerCase();
+		if(me.getType() == 0)
+		{
+			if(message.contains("anti-bot")) { randoms++; }
+			else if(message.contains(("silk"))) { money += 5120; steals++; }
+			else if(message.contains(("golden"))) { money += 6000; steals++; }
+			else if(message.contains(("emerald"))) { money += 12000; steals++; }
+			else if(message.contains(("battlestaff"))) { money += 16000; steals++; }
+			else if(message.contains(("adamant"))) { money += 20000; steals++; }
+			else if(message.contains(("steal some coins")))
+			{
+				Item cash = Inventory.getItem(996);
+				if(cash != null)
+				{
+					int gained = cash.getStackSize();
+					money += gained - oldstack;
+					if(gained > 2000000000)
+					{
+						Menu.sendAction(493, cash.getId() - 1, cash.getSlot(), 3214);
+						Time.sleep(1000);
+						oldstack = 0;
+					}
+					else
+						oldstack = gained;
+				}
+				steals++;
+			}
+			else if(message.contains("coins."))
+			{
+				String values[] = message.split(" ");
+				money += Integer.parseInt(values[2]);
+				
+				Item cash = Inventory.getItem(996);
+				if(cash != null)
+				{
+					if(cash.getStackSize() > 2000000000)
+					{
+						Menu.sendAction(493, cash.getId() - 1, cash.getSlot(), 3214);
+						Time.sleep(1000);
+					}
+				}
+				steals++;
+			}
+			else if(message.contains("seed stall") || message.contains("wine stall") || message.contains("master farmer"))
+			{
+				steals++;
+			}
+		}
 	}
 
 	@Override
-	public void paint(Graphics arg0)
+	public void paint(Graphics graphics)
 	{
-		// TODO Auto-generated method stub
+		Graphics2D g = (Graphics2D)graphics;
+		g.drawImage(chat, 8, 345, null);
+		g.setFont(new Font("Verdana", 0, 12));
+		g.setColor(new Color(255, 255, 255));
+		
+		g.drawString("Runtime: " + runtime.toString(), 18, 366);
+		g.drawString("Steals: " + steals, 18, 384);
+		g.drawString("Steals/h: " + runtime.getPerHour(steals), 18, 402);
+		if(location != 2 && location != 3)
+		{
+			g.drawString("Money: " + formatNumber(money), 18, 420);
+			g.drawString("Money/h: " + formatNumber(runtime.getPerHour(money)), 18, 438);
+		}
+		g.drawString("Randoms: " + randoms, 18, (location != 2 && location != 3) ? 456 : 420);
+	}
+	
+	private Image getImage(String url)
+	{
+		try{ return ImageIO.read(new URL(url)); }
+		catch(Exception exception) { return null; }
+	}
+	
+	private String formatNumber(double number)
+	{
+		DecimalFormat format = new DecimalFormat("#,###.00");
+
+		if (number >= 1000 && number < 1000000) { return format.format(number / 1000) + "K"; }
+		else if (number >= 1000000 && number < 1000000000) { return format.format(number / 1000000) + "M"; }
+		else if (number >= 1000000000){ return format.format(number / 1000000000) + "B"; }
+		
+        return ("" + number);
 	}
 	
 	public class Deposit implements Strategy
